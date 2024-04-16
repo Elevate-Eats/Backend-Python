a80b9358746ebc734b5b04df06bce68573883bb0
Evan Pradipta Hardinatha <84654936+kleopasevan@users.noreply.github.com>
2024-04-16 20:44:59 +0700

    Add:reportService.Py Date as new Params
    Date will be new param

--- app/services/reportService.py
+++ app/services/reportService.py
@@ -5,7 +5,7 @@ class ReportService:
   def __init__(self):
     self.reportRepository = ReportRepository()
     
-  async def generateDailyReport(self, branchId: int):
-    reportData = await self.reportRepository.getDailyReportData(branchId)
+  async def generateDailyReport(self, branchId: int, date: str):
+    reportData = await self.reportRepository.getDailyReportData(branchId, date)
     pdfFile = await PDFGenerator.createPdf(reportData)
     return pdfFile
