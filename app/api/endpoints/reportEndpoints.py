301de3d485fa1756eba9be9ae7c72bb49a7ab5d9
Evan Pradipta Hardinatha <84654936+kleopasevan@users.noreply.github.com>
2024-04-16 20:48:59 +0700

    Update: ReportEndpoints
    Updating report endpoints response, and making it can handle pdf stream

--- app/api/endpoints/reportEndpoints.py
+++ app/api/endpoints/reportEndpoints.py
@@ -1,15 +1,25 @@
-from fastapi import APIRouter, Depends, HTTPException
+from fastapi import APIRouter, Depends, HTTPException, Response
 from app.services.reportService import ReportService as ReportServiceClass
+from fastapi.responses import StreamingResponse
+import io
 
 router = APIRouter()
 
 def get_report_service():
-    return ReportServiceClass()
+  return ReportServiceClass()
 
-@router.post("/generate-daily-report/")
-async def generateDailyReport(branchId: int, reportService: ReportServiceClass = Depends(get_report_service)):
+@router.get("/generate-daily-report/", response_class=Response, responses={
+  200: {
+    "description": "Returns the daily report as a PDF",
+    "content": {"application/pdf": {}}
+  },
+  500: {
+    "description": "Internal Server Error"
+  }
+})
+async def generateDailyReport(branchId: int, date: str, reportService: ReportServiceClass = Depends(get_report_service)):
   try:
-    result = await reportService.generateDailyReport(branchId)
-    return {"message": "Report generated succesfully", "data":result}
+    result = await reportService.generateDailyReport(branchId, date)
+    return StreamingResponse(io.BytesIO(result), media_type="application/pdf")
   except Exception as e:
     raise HTTPException(status_code=500, detail=str(e))
